c36d27ca3aabd932e1efa71c519ab93148f7f714
starpact <yhj.await@gmail.com>
2024-01-30 16:44:13 +0800

    include hidden files but not .git

--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,34 @@ use ignore::{WalkBuilder, WalkState};
 
 fn main() {
     let from_file = std::env::args().nth(1).expect("no start path given");
-    let from_file = canonicalize(Path::new(&from_file)).unwrap();
+    let from_file = canonicalize(from_file).unwrap();
     assert!(from_file.is_file());
     let from_dir = from_file.parent().unwrap();
     assert!(from_dir.is_dir());
+    let git_dir = &canonicalize(".git").unwrap();
+    assert!(git_dir.is_dir());
 
     let pwd = &std::env::current_dir().unwrap();
 
     for ret in WalkBuilder::new(from_dir)
+        .hidden(false)
         .sort_by_file_path(Path::cmp)
         .build()
     {
         let entry = ret.unwrap();
-        if entry.file_type().unwrap().is_file() {
+        if entry.file_type().unwrap().is_file() && !entry.path().starts_with(git_dir) {
             println!("{}", entry.path().strip_prefix(pwd).unwrap().display());
         }
     }
 
-    let walker = WalkBuilder::new(pwd).build_parallel();
+    let walker = WalkBuilder::new(pwd).hidden(false).build_parallel();
     walker.run(|| {
         Box::new(move |ret| match ret {
             Ok(entry) => {
-                if entry.file_type().unwrap().is_file() && !entry.path().starts_with(from_dir) {
+                if entry.file_type().unwrap().is_file()
+                    && !entry.path().starts_with(git_dir)
+                    && !entry.path().starts_with(from_dir)
+                {
                     println!("{}", entry.path().strip_prefix(pwd).unwrap().display());
                 }
                 WalkState::Continue
